24652964742d8ada3be1419962ad42fe8faada4b
Piyush Lahoti <maheswaripiyush9@gmail.com>
2025-06-07 14:56:07 +0530

    Added move to zero problem

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,14 @@
 package org.example;
 
+import org.example.arrays.MoveZeroToLeft;
+
 public class Main {
     public static void main(String[] args) {
+        int[] arr = {3, 2, 1, 0, 0, 2, 0, 1};
+        MoveZeroToLeft.moveZeroToLeft(arr);
+        for (int ele : arr) {
+            System.out.print(ele + " ");
+        }
+        System.out.println();
     }
 }
---
+++ src/main/java/org/example/arrays/MoveZeroToLeft.java
@@ -0,0 +1,27 @@
+package org.example.arrays;
+
+public class MoveZeroToLeft {
+
+    /**
+     *
+     * @param arr
+     * Instead of shifting elements one by one (which is slow), we use a two-pointer approach from the end of the array:
+     *      One pointer (i) goes through every element from the end to the start.
+     *      The other pointer (j) tracks where to place the next non-zero element.
+     * When we find a non-zero element, we swap it with the element at position j.
+     * This pushes the non-zero elements toward the end of the array and leaves the beginning (the left side) for zeros.
+     */
+    public static void moveZeroToLeft(int[] arr) {
+        int i = arr.length - 1;
+        int j = arr.length - 1;
+        while (i >= 0) {
+            if (arr[i] != 0) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j--;
+            }
+            i--;
+        }
+    }
+}
