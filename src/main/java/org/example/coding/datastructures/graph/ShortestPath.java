d72409b84af3633a178d8d2209ef5ef5411282ad
Piyush Lahoti <maheswaripiyush9@gmail.com>
2025-07-07 00:54:07 +0530

    Graph | Shortest Path

---
+++ src/main/java/org/example/coding/datastructures/graph/ShortestPath.java
@@ -0,0 +1,46 @@
+package org.example.coding.datastructures.graph;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ShortestPath {
+    static class Pair {
+        int node;
+        int level;
+
+        public Pair(int node, int level) {
+            this.node = node;
+            this.level = level;
+        }
+    }
+
+    public int[] shortestPath(ArrayList<ArrayList<Integer>> adj, int src) {
+        // code here
+        int n = adj.size();
+        boolean vis[] = new boolean[n];
+        int[] dis = new int[n];
+        Arrays.fill(dis, Integer.MAX_VALUE);
+        Queue<Pair> q = new LinkedList<>();
+        q.offer(new Pair(src, 0));
+        vis[src] = true;
+        dis[src] = 0;
+        while (!q.isEmpty()) {
+            int node = q.peek().node;
+            int level = q.peek().level;
+            q.poll();
+            dis[node] = Math.min(dis[node], level);
+            for (int ele : adj.get(node)) {
+                if (!vis[ele]) {
+                    vis[ele] = true;
+                    q.offer(new Pair(ele, level + 1));
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (dis[i] == Integer.MAX_VALUE) dis[i] = -1;
+        }
+        return dis;
+    }
+}
