940508858fc39fb37934274805a8a8a0d755f5e9
Piyush Lahoti <maheswaripiyush9@gmail.com>
2025-08-03 00:49:03 +0530

    LLD: Adapter Pattern

---
+++ src/main/java/org/example/lld/patterns/structural/adapter/AudioPlayer.java
@@ -0,0 +1,13 @@
+package org.example.lld.patterns.structural.adapter;
+
+public class AudioPlayer {
+    private MediaPlayer mediaPlayer;
+
+    public AudioPlayer(MediaPlayer mediaPlayer) {
+        this.mediaPlayer = mediaPlayer;
+    }
+
+    public void play(String audioType, String fileName) {
+        mediaPlayer.play(audioType, fileName);
+    }
+}
---
+++ src/main/java/org/example/lld/patterns/structural/adapter/MediaPlayer.java
@@ -0,0 +1,5 @@
+package org.example.lld.patterns.structural.adapter;
+
+public interface MediaPlayer {
+    void play(String audioType, String fileName);
+}
